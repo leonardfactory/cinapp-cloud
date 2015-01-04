0339113439aa611855d172b346cd663fe9542506
Leonardo Ascione <me@leonardfactory.com>
2015-01-04 18:18:03 +0100

    Check if movie exists before adding it again

--- cloud/main.js
+++ cloud/main.js
@@ -3,24 +3,36 @@ ParseCustom.init();
 
 var Movie = require('cloud/models/Movie.js');
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
+/**
+ * Add a new movie if it doesn't exist in Database.
+ */
 Parse.Cloud.define("insertMovie", function(request, response) 
 {
-    var movie = new Movie();
-    movie.fromObject(request.params.movie);
+    var movieExistsQuery = new Parse.Query(Movie);
+    movieExistsQuery.equalTo('imdbId', request.params.movie.imdbId);
+    movieExistsQuery.find()
+        .then(function (movies) 
+        {
+            if(movies.length > 0) {
+                response.success(movies[0]);
+            }
+            else {
+                var movie = new Movie();
+                movie.fromObject(request.params.movie);
     
-    //var acl = new Parse.ACL();
-    //acl.setPublicReadAccess(true);
-    //acl.setRoleWriteAccess("Administrator", true);
+                //var acl = new Parse.ACL();
+                //acl.setPublicReadAccess(true);
+                //acl.setRoleWriteAccess("Administrator", true);
     
-    movie.save(null, { 
-        useMasterKey: true,
-        success: function (object) {
-            response.success(object);
-        },
-        error: function (error) {
-            response.error(object);
-        }
-    });
+                movie.save(null, { 
+                    useMasterKey: true,
+                    success: function (object) {
+                        response.success(object);
+                    },
+                    error: function (error) {
+                        response.error(object);
+                    }
+                });
+            }
+        });
 });
